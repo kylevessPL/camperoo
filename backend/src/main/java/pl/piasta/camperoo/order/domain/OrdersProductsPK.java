dd6c36653c064263492325deeef5ff2db277d97c
Kacper Piasta <kacperpiasta@gmail.com>
2022-11-15 03:19:07 +0100

    override equals and hashCode methods in OrdersProductsPK
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- backend/src/main/java/pl/piasta/camperoo/order/domain/OrdersProductsPK.java
+++ backend/src/main/java/pl/piasta/camperoo/order/domain/OrdersProductsPK.java
@@ -1,10 +1,13 @@
 package pl.piasta.camperoo.order.domain;
 
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
+@EqualsAndHashCode
 public class OrdersProductsPK implements Serializable {
     @Column(nullable = false)
     private Long orderId;
