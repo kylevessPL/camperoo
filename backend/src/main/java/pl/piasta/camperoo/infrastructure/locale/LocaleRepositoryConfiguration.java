8fbec22b8efda024060800604cbaaeb9fe722644
Kacper Piasta <kacperpiasta@gmail.com>
2022-11-24 22:50:50 +0100

    scan repository classes
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- backend/src/main/java/pl/piasta/camperoo/infrastructure/locale/LocaleRepositoryConfiguration.java
+++ backend/src/main/java/pl/piasta/camperoo/infrastructure/locale/LocaleRepositoryConfiguration.java
@@ -1,10 +1,12 @@
 package pl.piasta.camperoo.infrastructure.locale;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import pl.piasta.camperoo.global.domain.LocaleRepository;
 
 @Configuration
+@ComponentScan
 class LocaleRepositoryConfiguration {
     @Bean
     LocaleRepository localeRepository(LocaleJpaRepository jpaRepository) {
--- backend/src/main/java/pl/piasta/camperoo/infrastructure/order/OrderRepositoryConfiguration.java
+++ backend/src/main/java/pl/piasta/camperoo/infrastructure/order/OrderRepositoryConfiguration.java
@@ -1,10 +1,12 @@
 package pl.piasta.camperoo.infrastructure.order;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import pl.piasta.camperoo.order.domain.OrderRepository;
 
 @Configuration
+@ComponentScan
 class OrderRepositoryConfiguration {
     @Bean
     OrderRepository orderRepository(OrderJpaRepository jpaRepository) {
