e6a02e4188de9f75eba5d9281c7d4aa3431709c3
Kacper Piasta <kacperpiasta@gmail.com>
2022-11-27 09:40:05 +0100

    EmailNotifier changes
    Signed-off-by: Kacper Piasta <kacperpiasta@gmail.com>

--- backend/src/main/java/pl/piasta/camperoo/infrastructure/email/EmailTemplates.java
+++ backend/src/main/java/pl/piasta/camperoo/infrastructure/email/EmailTemplates.java
@@ -1,10 +1,11 @@
 package pl.piasta.camperoo.infrastructure.email;
 
-import lombok.experimental.UtilityClass;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-@UtilityClass
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 class EmailTemplates {
-    public final String ORDER_INVOICE = "order-invoice-email";
-    public final String ORDER_STATUS_CHANGE = "order-status-change-email";
-    public final String PASSWORD_RESET = "password-reset-email";
+    public static final String ORDER_INVOICE = "order-invoice-email";
+    public static final String ORDER_STATUS_CHANGE = "order-status-change-email";
+    public static final String PASSWORD_RESET = "password-reset-email";
 }
